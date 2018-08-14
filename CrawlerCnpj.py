3997886cc2674186381e99e67170880ddd5fbefd
Diogo Bersuc <dbersuc@gmail.com>
2018-08-14 20:08:35 +0000

    Atualização de verificação do status do servidor

--- CrawlerCnpj.py
+++ CrawlerCnpj.py
@@ -11,16 +11,20 @@ for i in lines:
     print("Fazendo o # {} de um total {}".format(a, lines.__len__()))
     url1 = "https://www.receitaws.com.br/v1/cnpj/" + i
     response = requests.get(url1)
+    while response.status_code != 200:
+        repouso = 30 #em segundos
+        print("Aguardando liberação do servidor... Testaremos de novo em {} segundos".format(repouso))
+        time.sleep(repouso)
+        response = requests.get(url1)
+    print('Status code: {}\n Horário: {}'.format(response.status_code,time.ctime()))
     todos = json.loads(response.text)
-    print('{} {}'.format(response,time.ctime()))
     fantasia = todos['nome']
     print(fantasia)
     emails = todos['email']
     print(emails)
     textao.write('{},{}\n'.format(fantasia,emails))
     #aguarda 20 segs
-    time.sleep(20)
+    time.sleep(30)
     a = a + 1
-
 
 
