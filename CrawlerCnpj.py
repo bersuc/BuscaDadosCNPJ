9e267ebf8a4e2dc12a0bfe486baf92ed764b1fed
Diogo Bersuc <dbersuc@gmail.com>
2018-08-15 13:19:27 +0000

    Ajustando Contagem e quebras de linhas

--- CrawlerCnpj.py
+++ CrawlerCnpj.py
@@ -8,23 +8,20 @@ textao = open('dados.txt','w')
 a = 1
 
 for i in lines:
-    print("Fazendo o # {} de um total {}".format(a, lines.__len__()))
+    print("Fazendo o # {} de um total {}".format(a, lines.__len__() -1 ))
     url1 = "https://www.receitaws.com.br/v1/cnpj/" + i
     response = requests.get(url1)
-    while response.status_code != 200:
-        repouso = 30 #em segundos
-        print("Aguardando liberação do servidor... Testaremos de novo em {} segundos".format(repouso))
-        time.sleep(repouso)
-        response = requests.get(url1)
-    print('Status code: {}\n Horário: {}'.format(response.status_code,time.ctime()))
-    todos = json.loads(response.text)
-    fantasia = todos['nome']
-    print(fantasia)
-    emails = todos['email']
-    print(emails)
-    textao.write('{},{}\n'.format(fantasia,emails))
-    #aguarda 20 segs
-    time.sleep(30)
-    a = a + 1
+    sc = response.status_code
+    if sc == 200:      
+        print('\nStatus code: {}\nHorário: {}'.format(response.status_code,time.ctime()))
+        todos = json.loads(response.text)
+        fantasia = todos['nome']
+        print(fantasia)
+        emails = todos['email']
+        print(emails + "\n")
+        textao.write('{},{}'.format(fantasia,emails))
+        #aguarda 20 segs
+        time.sleep(20)
+        a = a + 1
 
 
