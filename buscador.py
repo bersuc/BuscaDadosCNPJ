32aebd73ad680316e4e3f4e044d47134c65cdf7e
Bersuc <dbersuc@gmail.com>
2019-10-28 23:31:58 -0300

    fixes

--- buscador.py
+++ buscador.py
@@ -24,22 +24,45 @@ class Buscador:
     """
     Fecha os arquivos abertos anteriormente
     """
-
     def fechar(self):
         self.arq.close()
         self.textao.close()
         self.errosCNPJ.close()
-        print('Processo concluído com sucesso!')
+        print('Arquivos fechados com sucesso!')
 
     def pegaJson(self, cnpj):
         self.contador
         url = "https://www.receitaws.com.br/v1/cnpj/" + cnpj
         response = requests.get(url)
-        # sc = response.status_code
-        todos = json.loads(response.text)
-        return todos
+        sc = response.status_code
+        if sc == 200:
+            print('Status code: {}\nHorário: {}'.format(
+                response.status_code, time.ctime()))
+            todos = json.loads(response.text)
+            # adicionando um try pois pode não existir o CNPJ
+            try:
+                dados = {
+                    "Nome": todos['nome'],
+                    "Atividade": todos['atividade_principal'][0]['text'],
+                    "CNAE": todos['atividade_principal'][0]['code']
+                }
+            except ValueError:
+                # chamar log de erro para gravar CNPJ
+                print('############################################\n')
+                print('Erro ao obrter informações do cnpj')
+                print('############################################\n')
+                # gravaErros(cnpj)
+        else:
+            print('Aguardando 5 segundos. Motivo Status Code não é 200: {}'
+                  .format(sc))
+            # descansa()
+            # pegaJson(cnpj)
+        return dados
 
 
 if __name__ == "__main__":
+    inicio = time.time()
     var = Buscador().pegaJson("27865757000102")
+    fim = time.time()
+    print(fim - inicio)
     print(var)
